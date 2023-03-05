dd76d605a374e1ed42322e3da701a79863236b36
임두현 <dhlim07@naver.com>
2023-03-06 08:12:58 +0900

    add 0303 solve

--- BAEKJOON/2410/s1.py
+++ BAEKJOON/2410/s1.py
@@ -2,5 +2,13 @@ N = int(input())
 
 dp = [0] * (N+1)
 dp[1] = 1
-dp[2] = 2
-for a in range(1, N+1):
+
+for n in range(2, N+1):
+    a = 0
+    while n > 2**a:
+        a += 1
+    if n % 2:
+        dp[n] = dp[n-1]
+    else:
+        dp[n] = dp[n-1] + dp[n//2] 
+print(dp[N] % 1000000000)
