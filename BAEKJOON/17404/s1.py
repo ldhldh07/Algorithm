bad11cd50c180f33b6f8c32b7ac24fa69250d53b
임두현 <dhlim07@naver.com>
2023-03-13 12:40:55 +0900

    add 03/13 solve

--- BAEKJOON/17404/s1.py
+++ BAEKJOON/17404/s1.py
@@ -2,40 +2,25 @@ N = int(input())
 
 first_R, first_G, first_B = map(int, input().split())
 
-dp = [[[0 for _ in range(3)] for _ in range(3)] for _ in range(N)]
+dp = [[[1001 for _ in range(N)] for _ in range(3)] for _ in range(3)]
 
 # 첫번째 dp에 넣어주기
 for first_color in range(3):
     for dp_color in range(3):
-        dp[first_color][dp_color][0] = [first_R, first_G, first_B][dp_color] 
-
-# dp = [[[26, 0, 0], [40, 0, 0], [83, 0, 0]], [[26, 0, 0], [40, 0, 0], [83, 0, 0]], [[26, 0, 0], [40, 0, 0], [83, 0, 0]]]
-
-second_R, second_G, second_B =  map(int, input().split())
-
-# 첫번째 dp에 따라 2번째 dp에는 처음에 들어간 색깔이 안들어가게 설정
-for first_color in range(3):
-    dp[first_color][0][1] = min(dp[first_color][1][0], dp[first_color][2][0]) + second_R
-    dp[first_color][1][1] = min(dp[first_color][0][0], dp[first_color][2][0]) + second_G
-    dp[first_color][2][1] = min(dp[first_color][0][0], dp[first_color][1][0]) + second_B
-    
-    for dp_color in range(3):
         if first_color == dp_color:
-            dp[first_color][dp_color][1] = 1001
+            dp[first_color][dp_color][0] = [first_R, first_G, first_B][dp_color] 
 
-print(dp)
-# dp = [[[26, 1001, 0], [40, 86, 0], [83, 83, 0]], [[26, 89, 0], [40, 1001, 0], [83, 83, 0]], [[26, 89, 0], [40, 86, 0], [83, 1001, 0]]]
-
-for i in range(2, N):
+for i in range(1, N):
 
     R, G, B =  map(int, input().split())
     for first_color in range(3):
+        for dp_color in range(3):
+            # 첫번째 dp에 따라 2번째 dp에는 처음에 들어간 색깔이 안들어가게 설정
+            if first_color == dp_color and i == 1:
+                dp[first_color][dp_color][i] = 1001
+                continue
         dp[first_color][0][i] = min(dp[first_color][1][i-1], dp[first_color][2][i-1]) + R
         dp[first_color][1][i] = min(dp[first_color][0][i-1], dp[first_color][2][i-1]) + G
         dp[first_color][2][i] = min(dp[first_color][0][i-1], dp[first_color][1][i-1]) + B
-
-
-
-print(dp)
 
 print(min(dp[0][1][N-1], dp[0][2][N-1], dp[1][0][N-1], dp[1][2][N-1], dp[2][0][N-1], dp[2][1][N-1]))
