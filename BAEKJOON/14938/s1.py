263b392bc62b64041785225efca0eeba064df7a9
임두현 <dhlim07@naver.com>
2023-03-22 09:26:47 +0900

    update BAEKJOON 14928 solve

--- BAEKJOON/14938/s1.py
+++ BAEKJOON/14938/s1.py
@@ -1,17 +1,19 @@
 
-def dijkstra(N, start, adj, d):
+def dijkstra(N, start, adj):
+    distance_list = [0] * (n + 1)
     for i in range(N+1):
-        d[i] = adj[start][i]
+        distance_list[i] = adj[start][i]
     visited = [start]
     for _ in range(N-1):
         min_i = 0
         for i in range(1, N+1):
-            if i not in visited and d[i] < d[min_i]:
+            if i not in visited and distance_list[i] < distance_list[min_i]:
                 min_i = i
         visited.append(min_i)
         for next_node in range(1, N+1):
             if 0 < adj[min_i][next_node] < 1501:
-                distance[next_node] = min(distance[next_node], distance[min_i] + adjList[min_i][next_node])
+                distance_list[next_node] = min(distance_list[next_node], distance_list[min_i] + adjList[min_i][next_node])
+    return distance_list
 
 
 n, m, r = map(int, input().split())
@@ -28,11 +30,11 @@ for i in range(1, n+1):
 
 max_item_sum = 0
 for i in range(1, n+1):
-    distance = [0] * (n + 1)
-    dijkstra(n, i, adjList, distance)
+    
+    distance_list = dijkstra(n, i, adjList)
     item_sum = 0
-    for i, dist in enumerate(distance):
-        if dist <= m:
+    for i, distance in enumerate(distance_list):
+        if distance <= m:
             item_sum += item_list[i-1]
     if item_sum > max_item_sum:
         max_item_sum = item_sum
