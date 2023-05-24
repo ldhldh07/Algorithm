8963e95154c7a551087575dbc830e0792e4d425d
두현 <109324599+ldhldh07@users.noreply.github.com>
2023-05-24 09:34:07 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1444 ms, Memory: 723048 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,31 @@
+import sys
+sys.setrecursionlimit(10**5)
+
+def dfs(node):
+    global cnt
+    cnt += 1
+    visited[node] = cnt
+    for next_node in adjList[node]:
+        if not visited[next_node]:
+            dfs(next_node)
+    return
+
+
+N, M, R = map(int, input().split())
+
+adjList = [[] for _ in range(N+1)]
+for _ in range(M):
+    u, v = map(int, input().split())
+    adjList[u].append(v)
+    adjList[v].append(u)
+
+for i in range(1, N+1):
+    adjList[i].sort()
+
+visited = [0]  * (N + 1)    
+cnt = 0
+dfs(R)
+
+for i, ans in enumerate(visited):
+    if i:
+        print(ans)
