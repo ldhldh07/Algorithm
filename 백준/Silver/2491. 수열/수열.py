561c71ecb3762bfaa8a20e10fe506e21df5da570
두현 <109324599+ldhldh07@users.noreply.github.com>
2023-05-24 09:18:46 +0900

    [Silver IV] Title: 수열, Time: 144 ms, Memory: 39868 KB -BaekjoonHub

---
+++ 백준/Silver/2491. 수열/수열.py
@@ -0,0 +1,19 @@
+N = int(input())
+
+arr = list(map(int, input().split()))
+
+plus = [0] * N
+minus = [0] * N
+plus[0] = minus[0] = 1
+
+for i in range(N-1):
+    if arr[i] == arr[i+1]:
+        plus[i+1] = plus[i] + 1
+        minus[i+1] = minus[i] + 1
+    elif arr[i] < arr[i+1]:
+        plus[i+1] = plus[i] + 1
+        minus[i+1] = 1
+    else:
+        plus[i+1] = 1
+        minus[i+1] = minus[i] + 1
+print(max(plus + minus))
