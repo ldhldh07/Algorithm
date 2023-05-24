9bbb7e9b5490b6b4713d22195f10645f9ad5e708
두현 <109324599+ldhldh07@users.noreply.github.com>
2023-05-24 09:21:17 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 548 ms, Memory: 141216 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+def dfs(sp):
+    global cnt
+    route.append(sp)
+    visited[sp] = 1
+    while route:
+        node = route.popleft()
+        cnt += 1
+        order[node] = cnt
+        for next_node in adjList[node]:
+            if not visited[next_node]:
+                route.append(next_node)
+                visited[next_node] = 1
+
+
+
+N, M, R = map(int, input().split())
+
+adjList = [[] for _ in range(N + 1)]
+visited = [0] * (N + 1)
+order = [0] * (N + 1)
+cnt = 0
+route = deque()
+
+for _ in range(M):
+    u, v = map(int, input().split())
+    
+    adjList[u].append(v)
+    adjList[v].append(u)
+
+for i in range(1, N + 1):
+    adjList[i].sort(reverse=True)
+
+dfs(R)
+
+print(*order[1:], sep=('\n'))
