f801964e347cd4431b05013570184fe451673835
두현 <109324599+ldhldh07@users.noreply.github.com>
2023-05-24 09:32:45 +0900

    [Gold IV] Title: 전생했더니 슬라임 연구자였던 건에 대하여 (Hard), Time: 960 ms, Memory: 142336 KB -BaekjoonHub

---
+++ 백준/Gold/14698. 전생했더니 슬라임 연구자였던 건에 대하여 （Hard）/전생했더니 슬라임 연구자였던 건에 대하여 （Hard）.py
@@ -0,0 +1,24 @@
+import heapq
+
+T = int(input())
+
+answer = []
+for t in range(T):
+    slime_length = int(input())
+    slime_list = list(map(int, input().split()))
+
+    heapq.heapify(slime_list)
+
+    cost = 1
+
+    for _ in range(slime_length - 1):
+        first_min = heapq.heappop(slime_list)
+        second_min = heapq.heappop(slime_list)
+
+        energy = first_min * second_min
+        heapq.heappush(slime_list, energy)
+        energy = (first_min * second_min) % 1000000007
+        cost = (cost * energy) % 1000000007
+
+    answer.append(cost)
+print(*answer, sep='\n')
