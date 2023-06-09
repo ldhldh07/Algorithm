a2abb3f49a3cb7c24f244adb2ca4e86508951d3d
두현 <109324599+ldhldh07@users.noreply.github.com>
2023-06-09 17:51:10 +0900

    [Gold I] Title: 소수의 곱, Time: 348 ms, Memory: 157220 KB -BaekjoonHub

---
+++ 백준/Gold/2014. 소수의 곱/소수의 곱.py
@@ -0,0 +1,18 @@
+import heapq
+
+K, N = map(int, input().split())
+prime_numbers = list(map(int, input().split()))
+
+priority_queue = []
+for prime_number in prime_numbers:
+    heapq.heappush(priority_queue, (prime_number, prime_number)) 
+
+for _ in range(N):
+    min_prime_number, first_prime_number = heapq.heappop(priority_queue)
+    for prime_number in prime_numbers:
+        if prime_number > first_prime_number: 
+            break
+        new_prime_number = min_prime_number * prime_number
+        heapq.heappush(priority_queue, (new_prime_number, prime_number))
+
+print(min_prime_number)
