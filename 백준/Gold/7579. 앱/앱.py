2efef5e35993edc4c26bd5336c799a3230c27af0
두현 <109324599+ldhldh07@users.noreply.github.com>
2023-06-09 12:21:44 +0900

    [Gold III] Title: 앱, Time: 2096 ms, Memory: 280996 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.py
+++ 백준/Gold/7579. 앱/앱.py
@@ -3,14 +3,10 @@ N, M = map(int, input().split())
 memories = list(map(int, input().split()))
 costs = list(map(int, input().split()))
 
-dp = [float('inf') for _ in range(M + 1)]
-dp[0] = 0
+dp = [0] + [10 ** 9 for _ in range(M)]
 
 for n in range(N):
     for m in range(M, -1, -1):
-        if m >= memories[n]:
-            dp[m] = min(dp[m], dp[m-memories[n]]+costs[n])
-        else:
-            dp[m] = min(dp[m], costs[n])
+        dp[m] = min(dp[m], (dp[m-memories[n]] if m >= memories[n] else 0) + costs[n])
 
 print(dp[M])
