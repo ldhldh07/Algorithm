547ee0f74e825820f71830e5a951181bf45c5c9d
임두현 <dhlim07@naver.com>
2023-07-04 14:37:39 +0900

    add BAEKJOON 1202 solve

---
+++ BAEKJOON/1202/s1.py
@@ -0,0 +1,17 @@
+import heapq
+
+N, K = map(int, input().split())
+jewel_list = sorted([(weight, -value) for weight, value in [map(int, input().split()) for _ in range(N)]])
+bag_list = sorted([int(input()) for _ in range(K)])
+
+total_value = 0
+capable_jewel = []
+
+for bag in bag_list:
+    while jewel_list and bag >= jewel_list[0][0]:
+        weight, value = heapq.heappop(jewel_list)
+        total_value -= value
+    if not jewel_list:
+        break
+
+print(total_value)
