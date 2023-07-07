586031713625249e468ab77b82eea3e58c537014
두현 <109324599+ldhldh07@users.noreply.github.com>
2023-07-07 12:49:00 +0900

    [Gold II] Title: 피자판매, Time: 492 ms, Memory: 218340 KB -BaekjoonHub

--- 백준/Gold/2632. 피자판매/피자판매.py
+++ 백준/Gold/2632. 피자판매/피자판매.py
@@ -2,8 +2,9 @@ from collections import defaultdict
 
 def get_sum_dict(pizzas, size, selling_size):
     sum_dict = defaultdict(int)
-    if sum(pizzas) <= selling_size:
-        sum_dict[sum(pizzas)] += 1
+    total_pizza_sum = sum(pizzas)
+    if total_pizza_sum <= selling_size:
+        sum_dict[total_pizza_sum] += 1
 
     for length in range(1, size):
         pizza_sum = sum(pizzas[:length])
@@ -17,20 +18,6 @@ def get_sum_dict(pizzas, size, selling_size):
     return sum_dict
 
 
-def find_size(sum_list, finding_size, start, end):
-    if start > end:
-        return -1
-
-    mid = (start + end) // 2
-
-    if sum_list[mid][0] == finding_size:
-        return mid
-    elif sum_list[mid][0] > finding_size:
-        return find_size(sum_list, finding_size, start, mid - 1)
-    else:
-        return find_size(sum_list, finding_size, mid + 1, end)
-
-
 pizza_size = int(input())
 m, n = map(int, input().split())
 first_pizza = [int(input()) for _ in range(m)]
@@ -38,15 +25,12 @@ second_pizza = [int(input()) for _ in range(n)]
 
 first_pizza_sum_dict = get_sum_dict(first_pizza, m, pizza_size)
 second_pizza_sum_dict = get_sum_dict(second_pizza, n, pizza_size)
-second_pizza_sum_list = sorted(second_pizza_sum_dict.items())
 
 ans = 0
 ans = ans + first_pizza_sum_dict[pizza_size] + second_pizza_sum_dict[pizza_size]
 
 for first_pizza_sum, first_pizza_sum_count in first_pizza_sum_dict.items():
     required_second_pizza_size = pizza_size - first_pizza_sum
-    index = find_size(second_pizza_sum_list, required_second_pizza_size, 0, len(second_pizza_sum_list)-1)
-    if index >= 0:
-        ans += first_pizza_sum_count * second_pizza_sum_list[index][1]
+    ans += first_pizza_sum_count * second_pizza_sum_dict[required_second_pizza_size]
 
 print(ans)
