f719107a663390755f5cc55dfc88d55460a2f576
임두현 <dhlim07@naver.com>
2023-07-15 23:22:03 +0900

    add BAEKJOON 2675 solve

--- BAEKJOON/2675/s1/src/Main.java
+++ BAEKJOON/2675/s1/src/Main.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Main {
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        for(int i = 0; i < T; i++) {
+            String input = br.readLine();
+            String inputData[] = input.split(" ");
+            int R = Integer.parseInt(inputData[0]);
+            String S = inputData[1];
+            for (char ch : S.toCharArray()) {
+                for (int ii = 0; ii < R; ii++) {
+                    bw.write(String.valueOf(ch));
+                }
+            }
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
