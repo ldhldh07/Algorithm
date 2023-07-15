ff99ad3283dac84847a4ccf5ee988be79103d02c
임두현 <dhlim07@naver.com>
2023-07-15 23:22:47 +0900

    add BAEKJOON 2793 solve

--- BAEKJOON/2793/s1/src/Main.java
+++ BAEKJOON/2793/s1/src/Main.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Main {
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        for(int i = 1;i <10; i++) {
+            int result = N * i;
+            System.out.println(N + " * " + Integer.toString(i) + " = " + Integer.toString(result));
+        }
+
+        sc.close();
+    }
 }
