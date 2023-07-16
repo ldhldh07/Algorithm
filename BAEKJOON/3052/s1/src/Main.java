b0a6a07a9c4d695f804a6acf4856f281f6940316
임두현 <dhlim07@naver.com>
2023-07-16 17:29:13 +0900

    add BAEKJOON 2052 solve

--- BAEKJOON/3052/s1/src/Main.java
+++ BAEKJOON/3052/s1/src/Main.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Main {
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Map<Integer, Integer> moduloCount = new HashMap<>();
+        for(int i = 0; i < 10; i++) {
+            int input = Integer.parseInt(br.readLine());
+            int modulo = input % 42;
+            Integer numCount = moduloCount.getOrDefault(modulo, 0);
+            moduloCount.put(modulo, numCount + 1);
+        }
+        bw.write(String.valueOf(moduloCount.size()));
+        bw.flush();
+        br.close();
+        bw.close();
+    }
 }
