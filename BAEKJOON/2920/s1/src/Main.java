32963d00ce8f29219b9643f41e3b3315b5e342a5
임두현 <dhlim07@naver.com>
2023-07-16 17:28:56 +0900

    add BAEKJOON 2920 solve

--- BAEKJOON/2920/s1/src/Main.java
+++ BAEKJOON/2920/s1/src/Main.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Main {
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] numArray = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        boolean isAscending = numArray[0] < numArray[1];
+        String answer = isAscending ? "ascending" : "descending";
+        for(int i = 2; i < 8; i++) {
+            if (numArray[i-1] < numArray[i] != isAscending) {
+                answer = "mixed";
+            }
+        }
+        bw.write(answer);
+        bw.flush();
+        br.close();
+        bw.close();
+    }
 }
