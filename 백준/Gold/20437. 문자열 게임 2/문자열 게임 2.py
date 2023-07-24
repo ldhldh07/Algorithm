8e98a61b13abbd49f2ca355cfa1be78e67105efb
두현 <109324599+ldhldh07@users.noreply.github.com>
2023-07-24 17:34:38 +0900

    [Gold V] Title: 문자열 게임 2, Time: 232 ms, Memory: 117040 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.py
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.py
@@ -11,24 +11,18 @@ for _ in range(T):
 
     char_dict = defaultdict(list)
     char_count = defaultdict(int)
-    max_diff = -1
+    max_diff = 0
     min_diff = length_word
-
     for index in range(length_word):
         char = W[index]
         char_dict[char].append(index)
         char_count[char] += 1
 
-        if char_count[char] < K:
-            continue
-        else:
-            start_index = char_dict[char][char_count[char]-K]
-            end_index = char_dict[char][-1]
-            if W[start_index] == W[end_index]:
-                max_diff = max(max_diff, end_index - start_index + 1)
-            min_diff = min(min_diff, end_index - start_index + 1)
-
-    ans_list.append([min_diff, max_diff] if max_diff != -1 else -1)
+        if char_count[char] >= K:
+            current_diff = index - char_dict[char][char_count[char]-K] + 1
+            max_diff = max(max_diff, current_diff)
+            min_diff = min(min_diff, current_diff)
+    ans_list.append([min_diff, max_diff] if max_diff else 0)
 
 for ans in ans_list:
-    print(*ans) if ans != -1 else print(-1)
+    print(*ans) if ans else print(-1)
