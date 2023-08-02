930474f4980f4342d045946935d80bc64e881cef
두현 <109324599+ldhldh07@users.noreply.github.com>
2023-08-02 14:09:55 +0900

    [Gold II] Title: 로봇 조종하기, Time: 588 ms, Memory: 213296 KB -BaekjoonHub

--- 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.py
+++ 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.py
@@ -6,16 +6,18 @@ dp = [[[0, 0] for _ in range(M)] for _ in range(N)]
 dp[0][0][0] = mars_map[0][0]
 dp[0][0][1] = mars_map[0][0]
 
-for j in range(1, M):
-    dp[0][j][0] = dp[0][j-1][0] + mars_map[0][j]
-    dp[0][j][1] = dp[0][j-1][1] + mars_map[0][j]
 
-for i in range(1, N):
-    dp[i][0][0] = max(dp[i-1][0]) + mars_map[i][0]
-    dp[i][M-1][1] = max(dp[i-1][M-1]) + mars_map[i][M-1]
+for i in range(N):
+    if not i:
+        for j in range(1, M):
+            dp[i][j][0] = dp[i][j-1][0] + mars_map[i][j]
+            dp[i][j][1] = dp[i][j-1][1] + mars_map[i][j]
+        continue
+    for j in range(M):
+        dp[i][j][0] = max(dp[i-1][j]) + mars_map[i][j]
+        dp[i][M-1-j][1] = max(dp[i-1][M-1-j]) + mars_map[i][M-1-j]
+        if j:
+            dp[i][j][0] = max(dp[i][j][0], dp[i][j-1][0] + mars_map[i][j])
+            dp[i][M-1-j][1] = max(dp[i][M-1-j][1], dp[i][M-1-j+1][1] + mars_map[i][M-1-j])
 
-    for j in range(1, M):
-        dp[i][j][0] = max(max(dp[i-1][j]), dp[i][j-1][0]) + mars_map[i][j]
-        dp[i][M-1-j][1] = max(max(dp[i-1][M-1-j]), dp[i][M-j][1]) + mars_map[i][M-1-j]
-
-print(max(dp[N-1][M-1]))
+print(dp[N-1][M-1][0])
