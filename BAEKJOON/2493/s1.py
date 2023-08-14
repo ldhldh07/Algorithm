80ed19e1966548ec6f53e6c5b41a430d5555d6af
임두현 <dhlim07@naver.com>
2023-08-14 12:56:48 +0900

    add BAEKJOON 2493 solve

---
+++ BAEKJOON/2493/s1.py
@@ -0,0 +1,32 @@
+import sys
+
+si = sys.stdin.readline
+
+N = int(si().strip())
+
+buildings = list(enumerate(map(int, si().strip().split())))
+
+stack = []
+
+'''
+(0, 6)
+(1, 9)
+(1, 9), (2, 5)
+(1, 9), (3, 7)
+(1, 9), (3, 7), (4, 4)
+
+
+'''
+
+answer = [0 for _ in range(N)]
+
+for building_index in range(N):
+    current_building = buildings[building_index]
+    while stack and stack[-1][1] < current_building[1]:
+        stack.pop()
+    if stack:
+        last_higher_building = stack[-1]
+        answer[building_index] = last_higher_building[0] + 1
+    stack.append(current_building)
+
+print(*answer, sep=" ")
