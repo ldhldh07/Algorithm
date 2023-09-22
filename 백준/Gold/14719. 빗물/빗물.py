b32124e46908a510bee8caab7b1655741d58febd
두현 <109324599+ldhldh07@users.noreply.github.com>
2023-09-22 14:21:19 +0900

    [Gold V] Title: 빗물, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -0,0 +1,24 @@
+import sys
+
+si = sys.stdin.readline
+
+H, W = map(int, si().strip().split())
+
+heights = list(map(int, si().strip().split()))
+limit_heights = [0 for _ in range(W)]
+
+limit_height = 0
+
+for h_index in range(W):
+    if limit_height < heights[h_index]:
+        limit_height = heights[h_index]
+    limit_heights[h_index] = limit_height
+
+limit_height = 0
+
+for h_index in range(W-1, 0, -1):
+    if limit_height < heights[h_index]:
+        limit_height = heights[h_index]
+    limit_heights[h_index] = min(limit_heights[h_index], limit_height)
+
+print(sum([l - h for l, h in zip(limit_heights, heights)]))
