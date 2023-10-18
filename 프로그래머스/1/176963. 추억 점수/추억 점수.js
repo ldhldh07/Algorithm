5ee9b83609f99a2ef072f7420140357f5a5841e0
두현 <109324599+ldhldh07@users.noreply.github.com>
2023-10-18 13:35:39 +0900

    [level 1] Title: 추억 점수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -1,14 +1,12 @@
 function solution(name, yearning, photo) {
-    var answer = [];
     let nameMap = new Map();
     for (let i=0; i<name.length; i++) {
         nameMap.set(name[i], yearning[i]);
     }
-    photo.forEach((p) => {
-        const score = p.reduce((acc, n) => {
+    const answer = photo.map((p) => {
+        return p.reduce((acc, n) => {
             return acc + (nameMap.get(n) || 0);
         }, 0)
-        answer.push(score);
     })
     return answer;
 }
